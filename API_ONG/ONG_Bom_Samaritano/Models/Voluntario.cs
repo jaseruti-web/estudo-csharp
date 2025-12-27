1768596c9ee827fd6f2efb511af2eae52071e024
jaseruti-web <ja.seruti@gmail.com>
2025-12-27 09:18:43 -0300

    Add properties to Voluntario class

--- API_ONG/ONG_Bom_Samaritano/Models/Voluntario.cs
+++ API_ONG/ONG_Bom_Samaritano/Models/Voluntario.cs
@@ -1,8 +1,15 @@
-﻿namespace ApiOng.DesviosUrbanos.Models;
-
 public class Voluntario
 {
     public Guid Id { get; set; }
+
     public string Nome { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+
+    // Perfil geral
+    public bool Ativo { get; set; } = true;
+    public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+
+    // Reputação (derivada, não manual)
+    public int NivelConfiabilidade { get; set; } // ex: 1 a 5
+    public int PontuacaoGeral { get; set; }      // calculada, não editável
 }
